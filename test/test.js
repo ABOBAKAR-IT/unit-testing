7eb58c6ec9be733e88dd4cba6927a63a6618d241
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2021-10-20 13:35:00 +0500

    check null value

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 
 var {sum}=require('../app')
-it("sum to number",()=>{
+it("sum to equel to 3",()=>{
   expect(sum(1, 2)).toBe(3);
 })
 
@@ -12,3 +12,31 @@ it("sum grater then 0",()=>{
   const result=sum(2,3)
   expect(result).toBeGreaterThan(0)
 })
+
+it("sum is number",()=>{
+  expect(sum(1, 2)).toBe(3);
+})
+
+
+
+// Truthiness #
+
+// In tests, you sometimes need to distinguish between
+//  undefined, null, and false, but you sometimes do not want to treat these differently. 
+//  Jest contains helpers that let you be explicit about what you want.
+
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
+
+test('null', () => {
+  const n = null;
+  expect(n).toBeNull();
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).not.toBeTruthy();
+  expect(n).toBeFalsy();
+});
+
